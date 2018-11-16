c287d5e62af2d60bc2102be7635383f5cbc58690
Jérémy FAYOLLE <jeremy.fayolle@production.infra>
2018-11-16 17:07:05 +0100

    add useQuery()

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
-import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Store } from 'redux';
 
-import User, { UserFilters } from '../common/User';
-import { StoreState, StoreSyncAction } from './store';
 import UserPage from './UserPage';
 import UserForm from './UserForm';
 
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,18 +1,62 @@
-import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, ApolloQueryResult } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import gql from 'graphql-tag';
 import * as React from 'react';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
 import * as ReactDOM from 'react-dom';
 
 import App from './App';
+import { DocumentNode } from 'apollo-link';
 
 const client = new ApolloClient({
   link: new HttpLink(),
   cache: new InMemoryCache()
 });
 
+const getUsersQuery = gql`
+  query {getUsers{id name}}
+`
+// client.query({ query: getUsersQuery }).then(console.log);
+
+const ApolloContext = React.createContext(null! as ApolloClient<any>);
+
+
+export function Test() {
+  const data = useQuery(getUsersQuery);
+  console.log(data);
+  return (<div />);
+}
+
+export function useQuery<T>(query: DocumentNode): T {
+  const client = React.useContext(ApolloContext);
+  const [state, setState] = React.useState<{ data: T, init: boolean }>({data: null!, init: false});
+  if (!state.init) {
+    setState({init: true, data: null!});
+    client.query<T>({query}).then(res => setState({data: res.data, init: true}));
+  }
+  return state.data;
+}
+
 ReactDOM.render(
-  <ApolloProvider client={client}>
-      <App />
-  </ApolloProvider>,
+  (
+    <ApolloContext.Provider value={client}>
+      <Test></Test>
+    </ApolloContext.Provider>
+  ),
+  // <ApolloProvider client={client}>
+  //   <Query query={getUsersQuery}>
+  //     {({ loading, error, data }) => {
+  //       if (loading) return <p>Loading...</p>;
+  //       if (error) return <p>Error :(</p>;
+
+  //       return data.getUsers.map(({name}: { name: string }) => (
+  //         <div key={name}>
+  //           <p>{name}</p>
+  //         </div>
+  //       ));
+  //     }}
+  //   </Query>
+  // </ApolloProvider>,
   document.getElementById('app')
 )
